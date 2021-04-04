4b12a22d65c5aa441c163dd760db00858437d0fa
Julia Beatriz Adão <jubza@edu.univali.br>
2021-04-04 18:01:58 -0300

    Add project files.

---
+++ RepresentacaoDeGrafos/Models/Aresta.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RepresentacaoDeGrafos.Models
+{
+    public class Aresta
+    {
+        public int Codigo { get; set; }
+
+        [Required]
+        public string Identificador { get; set; }
+
+        [Required]
+        public Vertice Antecessor { get; set; }
+
+        [Required]
+        public Vertice Sucessor { get; set; }
+
+        [Required]
+        [Range(1, 1000)]
+        public int Custo { get; set; }
+
+        [Required]
+        public bool EhOrdenado { get; set; }
+    }
+}
---
+++ RepresentacaoDeGrafos/Models/Vertice.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RepresentacaoDeGrafos.Models
+{
+    public class Vertice
+    {
+        public int Codigo { get; set; }
+
+        [Required]
+        [MinLength(1)]
+        public string Identificador { get; set; }
+    }
+}
